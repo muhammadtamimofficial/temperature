a7a5ff8336ee5ed51d0ff1bd46bdf3eba8e132c6
Muhammad Tamim <mdt40234@gmail.com>
2022-11-09 23:14:26 +0600

    add weather icon with dynamicly

--- js/app.js
+++ js/app.js
@@ -4,11 +4,8 @@ const tempLogo = document.getElementById('temp-logo')
 const cityName = document.getElementById('city-name')
 const temp = document.getElementById('temp')
 const position = document.getElementById('position')
-// console.log(input_feild);
 // api key
 const API_KEY = '05de733ce77a58492d3a02fdc5361868';
-// api link
-// https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric
 inputBtn.addEventListener('click', () => {
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${input_feild.value}&appid=${API_KEY}&units=metric`;
     fetch(url)
@@ -19,6 +16,8 @@ const displayData = (data) => {
     cityName.innerText = data.name;
     temp.innerText = data.main.temp;
     position.innerText = data.weather[0].main;
-    console.log(data.main.temp);
+    // temp logo set with url 
+    const url = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
+    tempLogo.setAttribute('src', url)
 
 }
