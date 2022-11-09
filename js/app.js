7bdc498dc3846a8d4a0910dc34a66ba5c2764ae8
Muhammad Tamim <mdt40234@gmail.com>
2022-11-10 00:04:10 +0600

    visit site add show Dhaka weather information and if click on 'Enter' button then show result

--- js/app.js
+++ js/app.js
@@ -6,12 +6,20 @@ const temp = document.getElementById('temp')
 const position = document.getElementById('position')
 // api key
 const API_KEY = '05de733ce77a58492d3a02fdc5361868';
-inputBtn.addEventListener('click', () => {
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${input_feild.value}&appid=${API_KEY}&units=metric`;
+
+// load data on api and convart json 
+const loadData = (inputData) => {
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputData}&appid=${API_KEY}&units=metric`;
     fetch(url)
         .then(res => res.json())
         .then(data => displayData(data))
+}
+inputBtn.addEventListener('click', () => {
+    loadData(input_feild.value);
+    // empty input feild 
+    input_feild.value = '';
 })
+// display api data 
 const displayData = (data) => {
     cityName.innerText = data.name;
     temp.innerText = data.main.temp;
@@ -19,6 +27,13 @@ const displayData = (data) => {
     // temp logo set with url 
     const url = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
     tempLogo.setAttribute('src', url)
-    // empty input feild 
-    input_feild.value = '';
 }
+// if click on 'Enter' button then search
+input_feild.addEventListener('keypress', (event) => {
+    if (event.key === "Enter") {
+        inputBtn.click();
+    }
+})
+
+// visit site add show Dhaka weather information 
+loadData('Dhaka');
